e57aa3fe22d1944fff543d3ded498069dd6a9931
bit <bit@DESKTOP-479PJ90>
2019-01-23 16:41:35 +0900

    20190123수업예제완료

---
+++ src/com/douzone/emaillist/daotest/EmaillistDaoTest.java
@@ -0,0 +1,22 @@
+package com.douzone.emaillist.daotest;
+
+import java.util.List;
+
+import com.douzone.emaillist.dao.EmaillistDao;
+import com.douzone.emaillist.vo.EmaillistVo;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		
+		getListTest();
+	}
+	
+	public static void getListTest() {
+		List<EmaillistVo> list= new EmaillistDao().getList();
+		for(EmaillistVo vo:list) {
+			System.out.println(vo);
+		}
+	}
+
+}
